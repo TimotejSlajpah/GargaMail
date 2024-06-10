4d6d1c3f1cf3de813cf3fc8e39f4fdb845752179
TimotejSlajpah <timi.slajpah@gmail.com>
2024-06-10 12:26:11 +0200

    Delete old gargamail directory

--- gargamail/login_back.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    $_SESSION["email"] = $_POST['email'];
-    $_SESSION["geslo"] = $_POST['geslo'];
-    header('Location: site.php');
-?>
--- gargamail/logout.php
+++
@@ -1,7 +0,0 @@
-    <?php
-        session_start();
-        $newURL = "login_front.php";
-        session_unset();
-        session_destroy();
-        header('Location:'. $newURL);
-    ?>
